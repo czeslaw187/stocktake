0e9c3acd2d74cb0802e107daae90eff9ef3753e1
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2025-02-24 23:07:03 +0000

    signout

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -3,11 +3,12 @@
 import { Navbar, Collapse, NavbarToggler, Nav, NavbarBrand } from "reactstrap";
 import NavbarItem from "./NavbarItem";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SignOut from "./SignOut";
 import { useRouter } from "next/navigation";
 import { setLogin } from "../lib/features/passSlice";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { clear_food } from "../lib/features/countSlice";
 
 const labels = ['FOOD', 'BEER', 'SPIRIT']
 
@@ -16,12 +17,14 @@ export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false)
     const admin = useSelector(state=>state.pass)
     const router = useRouter()
+    const dispatch = useDispatch()
 
     useEffect(()=>{
         if (!admin.isLogged) {
+            dispatch(clear_food())
             router.push('/')
         }
-    },[admin.isLogged])
+    },[admin])
 
     function toggle() {
         setIsOpen(!isOpen)
