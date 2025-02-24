ba9cb1fbc158c64755838c7947d1f079a4cdd42e
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2025-02-24 09:20:21 +0000

    created table counterusers

--- app/Main/[slug]/page.js
+++ app/Main/[slug]/page.js
@@ -1,6 +1,6 @@
 import NavBar from "@/app/Components/Navbar";
 import ItemPage from "../Components/ItemPage"
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default async function Items({params}) {
     const slug = (await params).slug
