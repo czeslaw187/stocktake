8960846c7b71426e4713b4665de65a1da0ac8c03
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2025-03-03 01:05:44 +0000

    improved responsive sign in page

--- app/page.js
+++ app/page.js
@@ -76,7 +76,7 @@ export default function Page() {
     }
     console.log(user, 'store')
     return(
-        <div className="flex justify-center items-center w-screen h-screen">
+        <div className="flex flex-row flex-wrap justify-center items-center w-screen h-full md:h-screen">
             <Form>
                 <FormGroup className="flex flex-col rounder-md shadow-lg shadow-slate-500 p-7 mx-2">
                     <Label className="text-center text-3xl mb-2">Sign In</Label>
