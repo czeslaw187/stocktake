05decd510584955875ca369571c989ba21af5725
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2025-03-04 21:10:08 +0000

    responsive style of last log

--- app/Main/Components/LastEntryComponent.js
+++ app/Main/Components/LastEntryComponent.js
@@ -4,13 +4,13 @@ export default function LastEntryComponent({el}) {
     const minute = new Date(parseInt(el.date)).getMinutes()
     console.log(el)
     return(
-        <li className="w-[90%] h-auto md:h-[3rem] rounded shadow-md shadow-gray-500 mx-auto my-3 grid grid-cols-1 md:grid-cols-4 justify-items-center place-items-center">
+        <li className="w-[90%] h-auto md:h-[3rem] text-sm md:text-lg rounded shadow-md shadow-gray-500 mx-auto my-3 grid grid-cols-2 md:grid-cols-4 justify-items-start md:justify-items-center place-items-center">
             <div>
                 <div>{day}</div>
                 <div>{hour}:{minute}</div>
             </div>
             <div>{el.name}</div>
-            <div>{el.product} {el.category}</div>
+            <div className="font-bold md:font-normal">{el.product} {el.category}</div>
             <div>{el.amount < 0 ? el.amount : `+${el.amount}`}</div>
         </li>
     )
